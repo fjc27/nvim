711896960bbf27e1c08cd604d5797f944e5e2eb0
Javier Celestino <francisco.j.celestino@.com>
2025-08-15 14:36:16 -0600

    Updated lsp-config.lua

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -21,6 +21,7 @@ return {
         "neovim/nvim-lspconfig",
         config = function()
             local capabilities = require("cmp_nvim_lsp").default_capabilities()
+            capabilities.offsetEncoding = { "utf-8" }
             local lspconfig = require("lspconfig")
             lspconfig.lua_ls.setup({
                 capabilities = capabilities
