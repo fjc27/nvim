e82a607a06145c175cd0d5088fb88dd92b3da03a
Javier Celestino <francisco.j.celestino@.com>
2025-08-15 16:00:57 -0600

    Tweaked vim options

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -1,14 +1,17 @@
-vim.cmd('vnoremap y "+y')
-vim.cmd('nnoremap yy "+yy')
-vim.cmd('nnoremap p "+p')
-vim.cmd('nnoremap P "+P')
-vim.cmd("set clipboard=unnamedplus")
+-- vim.cmd('vnoremap y "+y')
+-- vim.cmd('nnoremap yy "+yy')
+-- vim.cmd('nnoremap p "+p')
+-- vim.cmd('nnoremap P "+P')
 
-vim.cmd("set expandtab")
-vim.cmd("set tabstop=4")
-vim.cmd("set softtabstop=4")
-vim.cmd("set shiftwidth=4")
-vim.cmd("set number")
-vim.cmd("set relativenumber")
-vim.cmd("set cursorline")
 vim.g.mapleader = " "
+
+vim.opt.clipboard = "unnamedplus"
+
+vim.opt.expandtab = true
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.cursorline = true
